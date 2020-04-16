5562db8a98d97b4832c0bedf494be0e6c45ab15c
Lucranix28 <45935449+Lucranix28@users.noreply.github.com>
2020-04-16 16:34:58 +0700

    commit

---
+++ Source Code/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include "TUBES_Ngram.c"
+
+int state = 0;
+/*
+1. Input Text
+2. Input (N)-Gram
+3. Print Text
+4. Process
+5. Print Key and Value
+6. Generate N-Gram-ed Text
+
+0. Exit Program
+*/
+int main(){
+
+    do
+    {
+        /* code */ 
+         
+
+
+    } while (state != 0);
+    printf("Program Ended");
+    return 0;
+}
--- Source Code/test_andy.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#include "TUBES_Ngram.c"
-
-int main(){
-
-
-    return 0;
-}
